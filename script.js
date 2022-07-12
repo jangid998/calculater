1399a2c0b6532ce82d7f483a31aa8551299526d6
jangid998 <52499706+jangid998@users.noreply.github.com>
2022-07-12 12:34:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//function that display value
+function dis(val)
+{
+    document.getElementById("input").value+=val;
+
+}
+  
+//function that evaluates the digit and return result
+function solve()
+{
+    let x = document.getElementById("input").value;
+    let y = eval(x)
+    document.getElementById("input").value = y
+}
+  
+//function that clear the display
+function clr()
+{
+    document.getElementById("input").value = " ";
+}
+function c()
+{
+    document.getElementById("input").value
+}
